9fcf7c7ec6158eeda8951399f9f168112681e94f
Olga Romanova <olga.romanova0709@gmail.com>
2019-11-04 13:10:27 -0800

    fixed

--- app.py
+++ app.py
@@ -4,12 +4,7 @@ import mars_information
 
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars"
-mongo = PyMongo(app)
-
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars"
 
 
 @app.route("/")
